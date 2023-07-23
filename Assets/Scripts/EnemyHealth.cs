23df81f846cde9f1470864b4a49bed02df562c6b
Frannie <frannie094@hotmail.com>
2023-07-22 22:34:45 -0400

    FPS assets

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    bool isDead = false;
+    private CapsuleCollider capsuleCollider;
+    private Score score;
+
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        BroadcastMessage("OnDamageTaken");
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        
+        if (capsuleCollider != null && capsuleCollider.enabled)
+        {
+            capsuleCollider.enabled = !capsuleCollider.enabled;
+        }
+        score = GameObject.FindGameObjectWithTag("Player").GetComponent<Score>();
+        score.AddOne();
+
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI deadText;
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        scoreText.text = "Zombies: " + count + " / 14";
+    }
+
+    private void Update()
+    {
+        if (count >= 14)
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+            deadText.text = "Ganaste";
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+            deadText.text = "Menu";
+        }
+    }
+
+
+    public void AddOne()
+    {
+        count++;
+        scoreText.text = "Zombies: " + count + " / 14";
+    }
+}
